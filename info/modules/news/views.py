49d88f383e32c7cc3cf08918edf5376d77cccb19
shengjun <371673381@qq.com>
2018-06-01 17:46:50 +0800

    导入模型类和常量文件&加载favicon图标

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,4 +1,4 @@
-from flask import session, render_template
+from flask import session, render_template, current_app
 # 导入蓝图对象
 from . import news_blu
 
@@ -7,3 +7,12 @@ from . import news_blu
 def index():
     session['name'] = '2018'
     return render_template('news/index.html')
+
+
+# 加载项目小图标
+@news_blu.route('/favicon.ico')
+def favicon():
+    # 静态路径访问的默认实现，send_static_file,
+    # 把静态文件发给浏览器
+    return current_app.send_static_file('news/favicon.ico')
+
