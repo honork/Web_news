9a2ebf5c15ffb92c1d0045d91b8aef79998d4aef
shengjun <371673381@qq.com>
2018-06-04 17:18:22 +0800

    项目首页的用户登录状态展示

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,12 +1,42 @@
-from flask import session, render_template, current_app
+from flask import session, render_template, current_app, jsonify
 # 导入蓝图对象
 from . import news_blu
+# 导入User模型类
+from info.models import User
+# 导入自定义的状态码
+from info.utils.response_code import RET
 
 # 使用蓝图对象
 @news_blu.route('/')
 def index():
-    session['name'] = '2018'
-    return render_template('news/index.html')
+    """
+
+    展示用户登录信息:检查用户登录状态；
+    2、尝试从redis数据库中获取用户的缓存信息，user_id
+    3、判断获取结果是否存在
+    4、根据user_id查询myql数据库
+    5、判断查询结果，
+    :return:
+    """
+    # 从redis数据库中获取用户id
+    user_id = session.get('user_id')
+    # 如果user_id存在，查询mysql数据库，获取用户信息
+    user = None
+    if user_id:
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
+            return jsonify(errno=RET.DBERR,errmsg='查询数据失败')
+        # if user:
+        #     return user.to_dict()
+        # else:
+        #     return None
+    data = {
+        'user_info':user.to_dict() if user else None
+    }
+
+    return render_template('news/index.html',data=data)
 
 
 # 加载项目小图标
